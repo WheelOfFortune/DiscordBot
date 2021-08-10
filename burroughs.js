a742a43d1257966bb438ee408e8d97bf0a986438
Wheel Of Fortune <88688824+WheelOfFortune@users.noreply.github.com>
2021-08-10 14:50:33 -0400

    WoF added comment the main code of the bot
    I added comments to each part of the code, explain the objective of each one

--- burroughs.js
+++ burroughs.js
@@ -1,10 +1,12 @@
-const Discord = require ('discord.js')
-const client = new Discord.Client()
+//Our first move it declare the constants of Discord and client. *Obligatory*
+const Discord = require ('discord.js') // To import the discord.js expresions and functions
+const client = new Discord.Client() // This is how we talk to the "bot"
 
-const { token } = require('./config.json')
+const { token } = require('./config.json') // The token of the bot. Be careful with this password, any that have it can control the bot.
+// We require the token from a external file to have it secured.
 
-client.on('ready', () => {
+client.on('ready', () => { // Whit this code the console return a string when the bot is online.
     console.log('Hello Master')
 })
 
-client.login(token)
+client.login(token) // The bot sign in with it password
