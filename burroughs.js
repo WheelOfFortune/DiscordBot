b0fda54d69606359c001cf6be8952a69bcc7e2d8
Wheel Of Fortune <88688824+WheelOfFortune@users.noreply.github.com>
2021-08-10 15:29:40 -0400

    Added: Command logic & prefix
    I add the message/command logic, the "ping" command and the declaration of the prefix "!"

--- burroughs.js
+++ burroughs.js
@@ -5,8 +5,26 @@ const client = new Discord.Client() // This is how we talk to the "bot"
 const { token } = require('./config.json') // The token of the bot. Be careful with this password, any that have it can control the bot.
 // We require the token from a external file to have it secured.
 
-client.on('ready', () => { // Whit this code the console return a string when the bot is online.
+const prefix = "!"
+
+client.on('ready', () => { // The console return a string when the bot is online.
     console.log('Hello Master')
+})
+
+client.on('message', message =>{ // The bot can "hear" our messages
+    
+    if (!message.content.startsWith(prefix) || message.author.bot) return; // The bot only take the messages that starts with the prefix (!) or+
+    // the messages that the bot isn't the author
+  
+      const args = message.content.slice(prefix.length).split(/ +/)
+      const command = args.shift().toLowerCase() 
+  
+  if (command === 'ping') { // if the message is "ping" the bot send "pong" message
+    
+    message.channel.send("pong") // "message.channel.send" refer to send a message in the same channel that the command was send
+  
+  }
+
 })
 
 client.login(token) // The bot sign in with it password
